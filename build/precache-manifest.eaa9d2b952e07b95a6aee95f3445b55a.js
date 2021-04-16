ddbd16c191de41f427ad27dab741332c14ac5a0a
abhirup7 <abhirupsahoo@gmail.com>
2021-04-17 00:40:00 +0530

    initial commit

---
+++ build/precache-manifest.eaa9d2b952e07b95a6aee95f3445b55a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bae5589241f4da1ac08457e7937ab84d",
+    "url": "/myrobofriends/index.html"
+  },
+  {
+    "revision": "17bcffd1e911e6c9f116",
+    "url": "/myrobofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "b7b45e2cb767913c810e",
+    "url": "/myrobofriends/static/css/main.a096462c.chunk.css"
+  },
+  {
+    "revision": "17bcffd1e911e6c9f116",
+    "url": "/myrobofriends/static/js/2.685e0e91.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/myrobofriends/static/js/2.685e0e91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7b45e2cb767913c810e",
+    "url": "/myrobofriends/static/js/main.e6aaf505.chunk.js"
+  },
+  {
+    "revision": "865d13dfeed7e19a0f21",
+    "url": "/myrobofriends/static/js/runtime-main.315ed56b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/myrobofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
